c0b07729d35494fcdfac0c579b412da26eae2117
franzspiess <picnhepanchopincho@gmail.com>
2019-03-23 13:41:14 +0100

    Updates

---
+++ precache-manifest.3e8b5d595b9b34a678fe99d617c297f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5898efb7f0a1a3dff560",
+    "url": "/quiz/static/js/runtime~main.e6ae5a1e.js"
+  },
+  {
+    "revision": "723d627630148e7225c7",
+    "url": "/quiz/static/js/main.2c7f3aa6.chunk.js"
+  },
+  {
+    "revision": "51b6ef485eff2255b39e",
+    "url": "/quiz/static/js/2.ba44c21f.chunk.js"
+  },
+  {
+    "revision": "723d627630148e7225c7",
+    "url": "/quiz/static/css/main.c087a814.chunk.css"
+  },
+  {
+    "revision": "52931a809ab1ece8f2bd0098a9172e35",
+    "url": "/quiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quiz/precache-manifest.b76545d11ca7ae92afd3ae575936c6f3.js"
+  "/quiz/precache-manifest.3e8b5d595b9b34a678fe99d617c297f3.js"
 );
 
 workbox.clientsClaim();
