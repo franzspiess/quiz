0723bdea4ecb03e6749510ca185907cf7554d5b6
franzspiess <picnhepanchopincho@gmail.com>
2019-03-23 13:27:25 +0100

    Updates

---
+++ precache-manifest.f9a79938ac74e562c5b55c09ba35de70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5898efb7f0a1a3dff560",
+    "url": "/quiz/static/js/runtime~main.e6ae5a1e.js"
+  },
+  {
+    "revision": "3ab74818400999165924",
+    "url": "/quiz/static/js/main.0e3d5393.chunk.js"
+  },
+  {
+    "revision": "d5dfc4ac4304b9542566",
+    "url": "/quiz/static/js/2.55e5c13f.chunk.js"
+  },
+  {
+    "revision": "3ab74818400999165924",
+    "url": "/quiz/static/css/main.c087a814.chunk.css"
+  },
+  {
+    "revision": "e2824959101f4227c5c4852f777b964c",
+    "url": "/quiz/index.html"
+  }
+];
